7c0885104e876b8945805fce790c98c8fccff8f8
Mansi Tyagi <mansityagi0606@gmail.com>
2026-01-12 01:38:13 +0530

    Initial commit - MLflow enabled MLOps pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,120 +1,69 @@
-import os
-import sys
-from dataclasses import dataclass
-
-from catboost import CatBoostRegressor
-from sklearn.ensemble import (
-    AdaBoostRegressor,
-    GradientBoostingRegressor,
-    RandomForestRegressor,
-)
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+import mlflow
+import mlflow.sklearn
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from xgboost import XGBRegressor
-
-from src.exception import CustomException
-from src.logger import logging
-
-from src.utils import save_object, evaluate_models
-
-
-@dataclass
-class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts", "model.pkl")
-
+from catboost import CatBoostRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
 class ModelTrainer:
-    def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+
+    def evaluate(self, y_true, y_pred):
+        mae = mean_absolute_error(y_true, y_pred)
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        r2 = r2_score(y_true, y_pred)
+        return mae, rmse, r2
 
     def initiate_model_trainer(self, train_array, test_array):
-        try:
-            logging.info("Split training and test input data")
-            X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
-                test_array[:, -1]
-            )
-            models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
-            }
-            params = {
-                "Decision Tree": {
-                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                "Random Forest": {
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
-                "Gradient Boosting": {
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate': [.1, .01, .05, .001],
-                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
-                "Linear Regression": {},
-                "XGBRegressor": {
-                    'learning_rate': [.1, .01, .05, .001],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
-                "CatBoosting Regressor": {
-                    'depth': [6, 8, 10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "AdaBoost Regressor": {
-                    'learning_rate': [.1, .01, 0.5, .001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8, 16, 32, 64, 128, 256]
-                }
+        X_train, y_train = train_array[:, :-1], train_array[:, -1]
+        X_test, y_test = test_array[:, :-1], test_array[:, -1]
 
-            }
+        models = {
+            "LinearRegression": LinearRegression(),
+            "Lasso": Lasso(),
+            "Ridge": Ridge(),
+            "KNN": KNeighborsRegressor(),
+            "DecisionTree": DecisionTreeRegressor(),
+            "RandomForest": RandomForestRegressor(),
+            "XGBoost": XGBRegressor(),
+            "CatBoost": CatBoostRegressor(verbose=False),
+            "AdaBoost": AdaBoostRegressor()
+        }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                                                 models=models, param=params)
+        best_model = None
+        best_r2 = -999
 
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
+        for model_name, model in models.items():
 
-            ## To get best model name from dict
+            with mlflow.start_run(run_name=model_name, nested=True):
 
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            best_model = models[best_model_name]
+                model.fit(X_train, y_train)
 
-            if best_model_score < 0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
+                train_mae, train_rmse, train_r2 = self.evaluate(y_train, y_train_pred)
+                test_mae, test_rmse, test_r2 = self.evaluate(y_test, y_test_pred)
 
-            predicted = best_model.predict(X_test)
+                # Log hyperparameters
+                mlflow.log_param("model", model_name)
 
-            r2_square = r2_score(y_test, predicted)
-            return r2_square
+                # Log metrics
+                mlflow.log_metric("train_rmse", train_rmse)
+                mlflow.log_metric("test_rmse", test_rmse)
+                mlflow.log_metric("train_r2", train_r2)
+                mlflow.log_metric("test_r2", test_r2)
+                mlflow.log_metric("test_mae", test_mae)
 
+                # Log model
+                mlflow.sklearn.log_model(model, "model")
 
+                if test_r2 > best_r2:
+                    best_r2 = test_r2
+                    best_model = model
 
-
-
-        except Exception as e:
-            raise CustomException(e, sys)
+        return best_model, {"best_r2": best_r2}
